2e0c4fc98a48ac99b25e8d1d05d113e85fb7f2d7
plumskl0 <plumskl0@users.noreply.github.com>
2018-01-02 18:03:38 +0100

    Anpassung des ContextBuilders

--- src/mas_a3/CustomBuilder.java
+++ src/mas_a3/CustomBuilder.java
@@ -1,23 +1,43 @@
 package mas_a3;
 
 import repast.simphony.context.Context;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.grid.GridFactory;
+import repast.simphony.context.space.grid.GridFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.SimpleCartesianAdder;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridBuilderParameters;
+import repast.simphony.space.grid.SimpleGridAdder;
+import repast.simphony.space.grid.StrictBorders;
+import repast.simphony.space.grid.WrapAroundBorders;
 
 public class CustomBuilder implements ContextBuilder<Object> {
 
 	@Override
 	public Context build(Context<Object> context) {
 		context.setId("mas_a3");
-		
+
+		// Space und Grid
+		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
+		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace("space", context,
+				new SimpleCartesianAdder<>(), new repast.simphony.space.continuous.StrictBorders(), 50, 50);
+
+		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
+
+		Grid<Object> grid = gridFactory.createGrid("grid", context, new GridBuilderParameters<Object>(
+				new StrictBorders(), new SimpleGridAdder<Object>(), true, 50, 50));
+
 		// Ziele
-		
+
 		// Boten
-		
+
 		// Lieferungen
-		
+
 		// Verhandlung?
-		
-		
+
 		return context;
 	}
 
